c5bfcf48408779e0019f504bd7e304fd3468a4a4
yppelu <yppelu@gmail.com>
2024-02-07 16:37:28 +0700

    Add placeRandomShips method

--- src/classes/Player.js
+++ src/classes/Player.js
@@ -21,4 +21,18 @@ export default class Player {
     if (moveResult === 1) this.#isMyTurn = true;
     return moveResult;
   }
+
+  placeRandomShips() {
+    const ships = [2, 3, 3, 4, 5];
+    const directions = ['horizontal', 'vertical'];
+
+    for (let i = ships.length - 1; i >= 0; i--) {
+      let x, y, direction;
+      do {
+        y = Math.floor(Math.random() * this.#board.length);
+        x = Math.floor(Math.random() * this.#board[y].length);
+        direction = directions[Math.floor(Math.random() * 2)];
+      } while (!this.#board.placeShip(x, y, ships[i], direction));
+    }
+  }
 }
